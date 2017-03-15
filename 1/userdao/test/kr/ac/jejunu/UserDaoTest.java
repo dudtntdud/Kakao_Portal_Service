b45e52f2601a490aa589d032ed7d7e4d6bde5456
twer4774 <dudtntdud@gmail.com>
2017-03-15 21:48:19 +0900

    첫주

---
+++ 1/userdao/test/kr/ac/jejunu/UserDaoTest.java
@@ -0,0 +1,42 @@
+package kr.ac.jejunu;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by ncl on 2017-03-15.
+ */
+public class UserDaoTest {
+    @Test
+    public void get() throws SQLException, ClassNotFoundException {
+        Long id = 1L;
+        String name = "강석원";
+        String password = "1234";
+
+        UserDao userDao = new UserDao();
+        User user = userDao.get(id);
+        assertThat(id, is(user.getId()));
+        assertThat(name, is(user.getName()));
+        assertThat(password, is(user.getPassword()));
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        String name = "헐크";
+        String password = "1111";
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new UserDao();
+        Long id = userDao.add(user);
+        User resultUser = UserDao.get(id);
+        assertThat(id, is(resultUser.getId()));
+        assertThat(name, is(resultUser.getName()));
+        assertThat(password, is(resultUser.getPassword()));
+    }
+}
